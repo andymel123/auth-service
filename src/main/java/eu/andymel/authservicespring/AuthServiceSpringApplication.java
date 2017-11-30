41e79e1798196af9c2dc842764a367a2f4708b19
andymel <git@andymel.eu>
2017-11-30 17:39:20 +0100

    [AuthService] initial commit of spring boot app from start.spring.io

---
+++ src/main/java/eu/andymel/authservicespring/AuthServiceSpringApplication.java
@@ -0,0 +1,12 @@
+package eu.andymel.authservicespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/eu/andymel/authservicespring/AuthServiceSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package eu.andymel.authservicespring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuthServiceSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
