e448c35589991ad4b4dfff0480d82087313f82fb
andymel <git@andymel.eu>
2017-11-30 18:11:14 +0100

    [AuthService] just some docs what to do

--- src/main/java/eu/andymel/authservicespring/AuthServiceSpringApplication.java
+++ src/main/java/eu/andymel/authservicespring/AuthServiceSpringApplication.java
@@ -3,6 +3,35 @@ package eu.andymel.authservicespring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * ### Inspirations ### 
+ * The tutorial I have the delegation of authentication to social id providers from is 
+ * 1.) https://spring.io/guides/tutorials/spring-boot-oauth2/ 
+ * 
+ * For the JWT part I will try to get infos from 
+ * 2.) https://auth0.com/blog/implementing-jwt-authentication-on-spring-boot/
+ * 3.) http://www.svlada.com/jwt-token-authentication-with-spring-boot/#ajax-authentication
+ *  * 
+ * I need
+ * 	a) An authentication filter to issue JWTS to users sending credentials
+ * 		this should be done by (1) I just need to issue JWT instead of the session
+ * 
+ * 	b) An authorization filter?! to refresh JWTs
+ *  
+ * 	c) Configure WebSecurityConfigurerAdapter 
+
+ * 
+ * regarding 2a)
+ * In (1) I have all one OAuth2ClientAuthenticationProcessingFilter per id provider
+ * In (2) he builds a JWTAuthenticationFilter (extending UsernamePasswordAuthenticationFilter)
+ * Both (the OAUth.. as well as the UsernameP... filters) are extending AbstractAuthenticationProcessingFilter so I simply leave the combined filter from (1) intact.
+ * But (2) already includes the JWT issuing logic inside of his JWTAuthenticationFilter. I will not extend the OAuth filter like (2) does (because of composition over inheritance). 
+ * (3) already does composition:
+ * he builds an AjaxLoginProcessingFilter, also extending AbstractAuthenticationProcessingFilter. The consturctor gets custom made
+ * AuthenticationSuccessHandler and AuthenticationFailureHandler. 
+ * I use such an AuthenticationSuccessHandler and simply try to add it as handler to my OAuth filters 
+ */
+
 @SpringBootApplication
 public class AuthServiceSpringApplication {
 
